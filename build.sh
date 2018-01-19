d6c9b3e6c246b56ed76fd0525069cd99936555ed
iaroki <iaroki@protonmail.com>
2018-01-19 12:35:12 +0200

    prettifying

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "Cleaning HTML..."; rm -rfv *.html;
-echo "Cleaning DIRS..."; rm -rfv author category tag theme;
-echo "Generating content with Docker...";
+echo "==> Cleaning HTML..."; rm -rfv *.html;
+echo "==> Cleaning DIRS..."; rm -rfv author category tag theme;
+echo "==> Generating content with Docker...";
 docker run -it --rm --name=pelican -v $PWD:/home/iaroki/blog pelican content;
-echo "Cleaning cache..."; rm -rfv __pycache__;
-echo "DONE!"
+echo "==> Cleaning cache..."; rm -rfv __pycache__;
+echo "==> DONE!"
