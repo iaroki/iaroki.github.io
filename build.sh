6ecac9845da9d356fdf6d8282c57fcbcd72db532
iaroki <iaroki@protonmail.com>
2018-01-19 11:58:42 +0200

    /home/max/dev/zen/iaroki.github.io fix

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 rm -rf *.html
 rm -rf author category tag theme
-docker run -it --rm -v /home/max/dev/zen/iaroki.github.io:/home/iaroki/blog pelican content
+docker run -it --rm -v $PWD:/home/iaroki/blog pelican content
 rm -rf __pycache__
