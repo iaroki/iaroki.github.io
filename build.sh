c4fd7cc7e00480a3044822e72537d75e14241b74
iaroki <iaroki@protonmail.com>
2018-01-22 12:54:13 +0200

    :fix docker image name

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 echo "==> Cleaning HTML..."; rm -rfv *.html;
 echo "==> Cleaning DIRS..."; rm -rfv author category tag theme;
 echo "==> Generating content with Docker...";
-docker run -it --rm --name=pelican -v $PWD:/home/iaroki/blog pelican content;
+docker run -it --rm --name=pelican -v $PWD:/home/iaroki/blog iaroki/pelican content;
 echo "==> Cleaning cache..."; rm -rfv __pycache__;
 echo "==> DONE!"
