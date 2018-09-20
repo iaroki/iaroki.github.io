767efbdc8d4db7a1659e008ee6ba17a76da6a601
iaroki <iaroki@protonmail.com>
2018-09-20 10:29:03 +0300

    Migration to Hugo

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo "==> Cleaning HTML..."; rm -rfv *.html;
-echo "==> Cleaning DIRS..."; rm -rfv author category pages tag theme;
-echo "==> Generating content with Docker...";
-docker run -it --rm --name=pelican -v $PWD:/home/iaroki/blog iaroki/pelican content;
-echo "==> Cleaning cache..."; rm -rfv __pycache__;
+echo "==> Generating content with Docker..."
+docker run -it --rm --name=hugo -v $PWD:/home/iaroki/blog iaroki/hugo
 echo "==> DONE!"
